a1b03641e20701d110ca8ad13788899417c1c0af
Mileshaase <mileshaase.imperium@gmail.com>
2023-06-26 14:01:15 -0400

    Working Moped

--- Assets/Scripts/Moped.cs
+++ Assets/Scripts/Moped.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Moped : MonoBehaviour
 {
     public GameObject Parent;
+    public Rigidbody RB;
     public GameObject Director;
     public GameObject Player;
     public Transform HandleBars;
@@ -16,9 +18,18 @@ public class Moped : MonoBehaviour
     public GameObject RHand;
     public GameObject LHand;
 
-    public float speed;
+    public float accelerationSpeed = 5f;
+    public float brakeSpeed = 10f;
+    public float maxSpeed = 10f;
+
+    public InputActionProperty LeftHandTrigger;
+    public InputActionProperty RightHandTrigger;
+
     private Quaternion defaultRot;
-    private bool accelerating;
+    private float RtriggerValue;
+    private float LtriggerValue;
+    public bool Grabbed;
+
 
     void Start()
     {
@@ -27,27 +38,43 @@ public class Moped : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(HandleBars.localRotation.z);
-        //Debug.Log(FrontWheel.localRotation.x);
-        FrontWheel.localRotation = Quaternion.Euler(FrontWheel.localRotation.x, FrontWheel.localRotation.y, HandleBars.localRotation.y * 50);
-
-        if(accelerating)
+        if(Grabbed)
         {
-            Vector3 forwardDirection = FrontWheel.forward;
+            LtriggerValue = LeftHandTrigger.action.ReadValue<float>();
+            RtriggerValue = RightHandTrigger.action.ReadValue<float>();
+            //Debug.Log(LtriggerValue);
+            //Debug.Log(RtriggerValue);
 
-            // Calculate the new position based on the forward direction
-            Vector3 newPosition = transform.position + Director.transform.forward * speed * Time.deltaTime;
+            //Debug.Log(HandleBars.localRotation.z);
+            //Debug.Log(FrontWheel.localRotation.x);
+            FrontWheel.localRotation = Quaternion.Euler(FrontWheel.localRotation.x, FrontWheel.localRotation.y, HandleBars.localRotation.y * 50);
 
-            // Move the game object to the new position
-            Parent.transform.position = newPosition;
-            Debug.Log("Moving");
+            float acceleratorInput = RtriggerValue;
+            float brakeInput = LtriggerValue;
+
+            // Calculate acceleration force based on the accelerator input
+            float accelerationForce = acceleratorInput * accelerationSpeed;
+            
+            // Calculate brake force based on the brake input
+            float brakeForce = brakeInput * brakeSpeed;
+
+            // Apply the acceleration and brake forces to the car's rigidbody
+            RB.AddForce(Director.transform.forward * accelerationForce);
+            RB.AddForce(-Director.transform.forward * brakeForce);
+
+            // Limit the car's speed to the maximum speed
+            RB.velocity = Vector3.ClampMagnitude(RB.velocity, maxSpeed);
+            
+            // Rotate the car towards the direction of the front wheel
+            Parent.transform.localRotation = Quaternion.Euler(-90, 0, FrontWheel.transform.localRotation.z * 200);
+            Debug.Log(FrontWheel.transform.localRotation.z * 200);
         }
     }
 
     public void OnGrab()
     {
+        Grabbed = true;
         Debug.Log("Grabbed Handlebars");
-        Player.transform.SetParent(Parent.transform);
         RHand.SetActive(false);
         RHandOnBar.SetActive(true);
         LHand.SetActive(false);
@@ -56,8 +83,8 @@ public class Moped : MonoBehaviour
 
     public void OnRelease()
     {
+        Grabbed = false;
         Debug.Log("Released Handlebars");
-        Player.transform.SetParent(null);
         RHand.SetActive(true);
         RHandOnBar.SetActive(false);
         LHand.SetActive(true);
@@ -67,13 +94,18 @@ public class Moped : MonoBehaviour
 
     public void WhipThisShit()
     {
-        Debug.Log("Accelerating");
-        accelerating = true;
+        if(RtriggerValue > 0)
+        {
+            Debug.Log("Accelerating");
+        }
+        if(LtriggerValue > 0)
+        {
+            Debug.Log("Braking");
+        }
     }
 
     public void Slow()
     {
-        Debug.Log("Braking");
-        accelerating = false;
+        Debug.Log("Stopped");
     }
 }
