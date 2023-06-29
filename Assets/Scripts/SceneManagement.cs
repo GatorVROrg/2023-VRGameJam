fefd702e79b5037ed7571d2c34c3465c79476230
Mileshaase <mileshaase.imperium@gmail.com>
2023-06-29 13:34:01 -0400

    scenes

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -15,7 +15,7 @@ public class SceneManagement : MonoBehaviour
     }
     public void StartGame()
     {
-        SceneManager.LoadScene("Game Scene");
+        SceneManager.LoadScene("Level Generation Testing Scene");
     }
     public void Quit()
     {
